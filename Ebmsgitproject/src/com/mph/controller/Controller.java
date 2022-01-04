06ec79112d9f5902dc9a1b510467220b03f7c005
vijaya-chintapalli <97013776+vijaya-chintapalli@users.noreply.github.com>
2022-01-04 14:53:43 +0530

    Update Controller.java
    done of controller part

--- Ebmsgitproject/src/com/mph/controller/Controller.java
+++ Ebmsgitproject/src/com/mph/controller/Controller.java
@@ -1,8 +1,24 @@
 package com.mph.controller;
+package com.mph.controller;
+import java.util.Scanner;
+import com.mph.model.Calculator;
 
 public class Controller 
 {
-  System.out.println("This is controller part of Ebmsgitproject");
-  System.out.println("code started from here");
-
-}
+	  Calculator cal;
+	  public void addnumbers() 
+	   {
+	     System.out.println("This is controller part of Ebmsgitproject");
+	     System.out.println("code started from here");
+	     cal=new Calculator();
+	     int a,b;
+       Scanner sc = new Scanner(System.in);
+	     System.out.println("Enter first number");
+       a = sc.nextInt();
+	     cal.setA(a);
+	     System.out.println("Enter Second number");
+	     b = sc.nextInt();
+	     cal.setB(b);
+       System.out.println(cal);	        
+	    }	 
+	}
