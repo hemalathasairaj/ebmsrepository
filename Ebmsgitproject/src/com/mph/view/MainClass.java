58a45bb17d532b5a11368aa92f87de5aef79dd1e
hemalathasairaj <97013808+hemalathasairaj@users.noreply.github.com>
2022-01-04 12:56:39 +0530

    Update MainClass.java
    added scanner statements by hema

--- Ebmsgitproject/src/com/mph/view/MainClass.java
+++ Ebmsgitproject/src/com/mph/view/MainClass.java
@@ -1,5 +1,6 @@
 package com.mph.view;
 
+import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
@@ -9,6 +10,13 @@ public class MainClass {
 		system.out.println("Hello guys");
 		system.out.println("Welcome to EBMS");
 		
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		a = nextInt();
+		System.out.println("Enter Second number");
+		b = nextInt();
+
 	}
 
 }
