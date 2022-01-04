a96907f6c4e673add10fb84701ba10008bc29ed7
Balakrishnan3 <97013915+Balakrishnan3@users.noreply.github.com>
2022-01-04 14:54:42 +0530

    Update Calculator.java
    added model class by Bala

--- Ebmsgitproject/src/com/mph/model/Calculator.java
+++ Ebmsgitproject/src/com/mph/model/Calculator.java
@@ -1,5 +1,34 @@
 package com.mph.model;
 
 public class Calculator {
+	 private int  a;
+	 private  int b;
+	public Calculator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public Calculator(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	@Override
+	public String toString() {
+		return "Calculator [a=" + a + ", b=" + b + "]";
+	}
+	 
 
 }
+
