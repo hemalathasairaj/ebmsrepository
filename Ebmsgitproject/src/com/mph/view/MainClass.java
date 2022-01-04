c35c4d8d02c68fc9ae27bbfdb7a0e28dcf449df7
hemalathasairaj <97013808+hemalathasairaj@users.noreply.github.com>
2022-01-04 12:47:24 +0530

    Update MainClass.java

--- Ebmsgitproject/src/com/mph/view/MainClass.java
+++ Ebmsgitproject/src/com/mph/view/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       system.out.println("hello");
+      		system.out.println("hello");
 		system.out.println("Happy new year");
 		
 	}
