c0222840c43c8263c69c72cbe429e39a20cd16ec
vijaya-chintapalli <97013776+vijaya-chintapalli@users.noreply.github.com>
2022-01-04 12:46:19 +0530

    Update Controller.java
    Changes done by Vijaya

--- Ebmsgitproject/src/com/mph/controller/Controller.java
+++ Ebmsgitproject/src/com/mph/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.mph.controller;
 
-public class Controller {
+public class Controller 
+{
+  System.out.println("This is controller part of Ebmsgitproject");
+  System.out.println("code started from here");
 
 }
